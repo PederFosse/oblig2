f90dd525ca58127bdd7b8ef733d90a3b3a21a5ba
PederFosse <peder.fosse@gmail.com>
2021-02-18 13:55:28 +0100

    Initial commit

---
+++ src/main/java/oblig2/oblig2/Oblig2Application.java
@@ -0,0 +1,13 @@
+package oblig2.oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig2Application.class, args);
+    }
+
+}
---
+++ src/test/java/oblig2/oblig2/Oblig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oblig2.oblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
